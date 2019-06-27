c80cbc5ad67c9689a09422e51c48db775b2ebc2e
Your Name <you@example.com>
2019-06-27 15:30:55 -0300

    .

--- views/index.blade.php
+++ views/index.blade.php
@@ -5,10 +5,10 @@
 <div class="container">
 	<div class="row">
 		<div class="col-lg-6 col-sm-6 col-xs-12">
-			<a href="{{ route('marco500.produtos') }}" class="btn btn-primary btn-lg w-full" style="padding: 25px;">Produtos</a>
+			<a href="{{ route('marco500.produtos') }}" class="btn btn-primary btn-lg w-full" style="padding: 25px;">Vendas por Produto</a>
 		</div>  
 		<div class="col-lg-6 col-sm-6 col-xs-12">
-			<a href="{{ route('marco500.pedidos') }}" class="btn btn-primary btn-lg w-full" style="padding: 25px;">Pedidos</a>
+			<a href="{{ route('marco500.pedidos') }}" class="btn btn-primary btn-lg w-full" style="padding: 25px;">Vendas por Pedido</a>
 		</div> 
 	</div>
 </div>
